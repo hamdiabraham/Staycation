3c52bd9a79f02bd2ff22331847d8f9fa46c3a485
HamdiAbraham <64348632+hamdiabraham@users.noreply.github.com>
2020-12-04 17:37:52 +0700

    edit styling featured image

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -7,8 +7,9 @@ export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked}>
       <Fade bottom>
-        <h4 className="mb-3">Most Picked</h4>
+        <h4 className="mb-3"> Most Picked </h4>{" "}
         <div className="container-grid">
+          {" "}
           {props.data.map((item, index) => {
             return (
               <div
@@ -18,38 +19,39 @@ export default function MostPicked(props) {
                 <Fade bottom delay={500 * index}>
                   <div className="card card-featured">
                     <div className="tag">
-                      ${item.price}
+                      $ {item.price}{" "}
                       <span className="font-weight-light">
                         {" "}
-                        per {item.unit}
-                      </span>
-                    </div>
+                        per {item.unit}{" "}
+                      </span>{" "}
+                    </div>{" "}
                     <figure className="img-wrapper">
                       <img
                         src={item.imageUrl}
                         alt={item.name}
                         className="img-cover"
                       />
-                    </figure>
+                    </figure>{" "}
                     <div className="meta-wrapper">
                       <Button
                         type="link"
                         className="stretched-link d-block text-white"
                         href={`/properties/${item._id}`}
                       >
-                        <h5>{item.name}</h5>
-                      </Button>
+                        <h5> {item.name} </h5>{" "}
+                      </Button>{" "}
                       <span>
-                        {item.city}, {item.country}
-                      </span>
-                    </div>
-                  </div>
-                </Fade>
+                        {" "}
+                        {item.city}, {item.country}{" "}
+                      </span>{" "}
+                    </div>{" "}
+                  </div>{" "}
+                </Fade>{" "}
               </div>
             );
-          })}
-        </div>
-      </Fade>
+          })}{" "}
+        </div>{" "}
+      </Fade>{" "}
     </section>
   );
 }
